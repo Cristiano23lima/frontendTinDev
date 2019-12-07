dedf298c9524402ce33ddc8fddb82743236368f5
Cristiano Rodrigues <dj7cristiano@gmail.com>
2019-12-06 21:08:59 -0300

    Adicionando a funcionalidade de match

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import {Link} from 'react-router-dom';
+import io from 'socket.io-client';
 import "./Main.css";
 
 import api from '../services/api';
+import itsamatch from '../assets/itsamatch.png';
 
 export default function Main({ match }){//pega todos os parametros passado na rota main
     const [users, setUsers] = useState([]);
+    const [matchDev, setMatchDev] = useState();
     
     useEffect(() => {
         async function loadUsers(){
@@ -21,6 +24,16 @@ export default function Main({ match }){//pega todos os parametros passado na ro
         loadUsers();
     }, [match.params.id]) //primeiro parametro a função que eu quero executar, e o segundo parametro é quando eu quero executa-la
     
+    useEffect(() => {
+        const socket = io('http://localhost:3333', {
+            query: { user: match.params.id }
+        });
+
+        socket.on('match', dev => {
+            setMatchDev(dev);
+        })
+    }, [match.params.id])
+
     async function handleLike(id){
         await api.post(`/devs/${id}/likes`, null, {
             headers: {
@@ -70,6 +83,18 @@ export default function Main({ match }){//pega todos os parametros passado na ro
                     <div className="empty">Acabou :(</div>
                 )
             }
+
+            { matchDev && (
+                <div className="match-container">
+                    <img src={itsamatch} alt="It's a match" />
+                    
+                    <img className="avatar" src={matchDev.avatar} alt="Avatar" />
+                    <strong>{matchDev.name}</strong>
+                    <p>{matchDev.bio}</p>
+
+                    <button type="button" onClick={() => setMatchDev(null)}>FECHAR</button>
+                </div>
+            )}
         </div>
     );
 }
